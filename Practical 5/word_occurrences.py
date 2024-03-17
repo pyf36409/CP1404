7cedaca6e45055ce1dbce5a96ecac2df65b02f61
pyf36409 <yifupu392@gmail.com>
2024-03-18 07:59:42 +0800

    Practicals

---
+++ Practical 5/word_occurrences.py
@@ -0,0 +1,9 @@
+words = {}
+text = input("Text: ").lower().split()
+for word in text:
+    if word in words:
+        words[word] += 1
+    else:
+        words[word] = 1
+for word, count in words.items():
+    print(f"{word}: {count}")
