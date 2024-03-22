319fe2a416b85871b894e7f56ec6ecde29952f6c
pyf36409 <yifupu392@gmail.com>
2024-03-23 00:40:49 +0800

    Practicals

--- Practical_6/used_cars.py
+++ Practical_6/used_cars.py
@@ -4,7 +4,7 @@ Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
 """
 
-from prac.car import Car
+from Practical_6.car import Car
 
 
 def main():
